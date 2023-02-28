8cf4ad4056ac3b5f028ac3ead1e0b9843e975203
Muhammad Shoaib <107323928+melsayedshoaib@users.noreply.github.com>
2023-02-28 08:56:18 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -246,3 +246,25 @@ document.addEventListener("click", (e) => {
     document.querySelector(".popup-overlay").remove();
   }
 });
+
+// Select All Bullets
+
+const allBullets = document.querySelectorAll(".nav-bullets .bullet");
+
+// Select All Links
+
+const allLinks = document.querySelectorAll(".links a");
+
+function scrollToASection(elements) {
+  elements.forEach((ele) => {
+    ele.addEventListener("click", (e) => {
+      e.preventDefault();
+      document.querySelector(e.target.dataset.section).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+}
+
+scrollToASection(allBullets);
+scrollToASection(allLinks);
